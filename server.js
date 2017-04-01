fc5870036eb89ae85ba9a713f8fea6716bb566df
Jared Petersen <jpetersen11@wou.edu>
2017-03-31 22:37:15 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+let express = require('express');
+let app = express();
+let port = 9000;
+
+// Server any files in the public directory
+app.use(express.static('public'));
+
+console.log(`STARTING RASPI-LIVE-DASHBOARD AT PORT ${port}`);
+app.listen(port)
