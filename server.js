3cd9eef368344caf15b55755f3e2ff230b115cd3
Jared Petersen <jpetersen11@wou.edu>
2017-08-27 14:31:54 -0700

    Switched to hls.js with auth

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ let app = express();
 let port = 9000;
 
 // Server any files in the public directory
-app.use(express.static('public'));
+app.use(express.static('public', {extensions: ['html']}));
 
 console.log(`STARTING RASPI-LIVE-DASHBOARD AT PORT ${port}`);
 app.listen(port)
